6c530d9ec3ae6bc65b8b2cd5b9aa80a42e73faf6
Justin Yeh <fabeoulo@yahoo.com.tw>
2023-04-30 16:53:20 +0800

    Add nodejs.

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+var path = require('path');
+
+module.exports = function (grunt) {
+    // Project configuration.
+    grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+        bower: {
+            install: {
+                options: {
+                    copy: true,
+                    targetDir: './src/main/webapp/libs',
+                    layout: 'byType',
+                    install: true,
+                    verbose: false,
+                    prune: false,
+                    cleanTargetDir: false,
+                    cleanBowerDir: false,
+                    bowerOptions: {}
+                }
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-bower-task');
+};
